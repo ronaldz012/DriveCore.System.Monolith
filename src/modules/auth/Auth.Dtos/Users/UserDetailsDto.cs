8c5a10772d8a6bda1f93b0b6ca1e5a72ecdfa895
ronaldz012 <rzepita02@gmail.com>
2025-11-05 10:32:56 -0400

    Update: UserDetailsDto Update

--- src/modules/auth/Auth.Dtos/Users/UserDetailsDto.cs
+++ src/modules/auth/Auth.Dtos/Users/UserDetailsDto.cs
@@ -9,7 +9,7 @@ public class UserDetailsDto
     public string Username { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
-    public string FatherLastName { get; set; } = string.Empty;
-    public string MotherLastName { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
     
 }
