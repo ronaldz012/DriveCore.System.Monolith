8e58e13b6d77cca2da6a1e188d42b11e445bd597
ronaldz012 <rzepita02@gmail.com>
2026-04-14 15:59:25 -0400

    fix: description optional

--- src/modules/inventory/Inventory.Contracts/Dtos/Brands/CreateBrandDto.cs
+++ src/modules/inventory/Inventory.Contracts/Dtos/Brands/CreateBrandDto.cs
@@ -6,6 +6,5 @@ public class CreateBrandDto
 {
     [Required, MinLength(3), MaxLength(15)]
     public string Name { get; set; } = string.Empty;
-    [Required, MinLength(3), MaxLength(15)]
     public string Description { get; set; } = string.Empty;
 }
