ada74cb832554b2d4bf0162a0425ef10ec799083
ronaldz012 <rzepita02@gmail.com>
2026-04-16 21:03:55 -0400

    Update:Code and Sku for Products

---
+++ src/modules/inventory/Inventory.Data/Persistence/Migrations/20260415205643_int_code_and_sku.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory.Data.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class int_code_and_sku : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Sku",
+                table: "ProductVariants",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "InternalCode",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 4, 15, 20, 56, 42, 277, DateTimeKind.Utc).AddTicks(6152));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 4, 15, 20, 56, 42, 277, DateTimeKind.Utc).AddTicks(6042));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sku",
+                table: "ProductVariants");
+
+            migrationBuilder.DropColumn(
+                name: "InternalCode",
+                table: "Products");
+
+            migrationBuilder.UpdateData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 4, 7, 20, 41, 51, 639, DateTimeKind.Utc).AddTicks(4728));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 4, 7, 20, 41, 51, 639, DateTimeKind.Utc).AddTicks(4603));
+        }
+    }
+}
